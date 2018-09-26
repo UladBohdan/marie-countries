1b0bda62d959d2b47a52decd1cfbd8c7ca032475
uladbohdan <vlbohdan@gmail.com>
2018-09-26 08:54:27 +0300

    [Ulad] Canada - Ontario, Quebec

--- data.js
+++ data.js
@@ -21,6 +21,6 @@ var countries = {
     'Iran',
     'Denmark'
   ],
-  ulad: ['GB', 'PT', 'US', 'US-CA', 'US-WA', 'MX', 'CU', 'RU-KDA', 'GE', 'US-NV', 'US-MA', 'US-NY', 'US-OR', 'Canada', 'CA-BC', 'US-HI'],
+  ulad: ['GB', 'PT', 'US', 'US-CA', 'US-WA', 'MX', 'CU', 'RU-KDA', 'GE', 'US-NV', 'US-MA', 'US-NY', 'US-OR', 'Canada', 'CA-BC', 'US-HI', 'CA-ON', 'CA-QC'],
   marie: ['IT', 'NO', 'TR', 'RU-KR', 'Finland', 'RU-MUR', 'RU-PSK', 'RU-NGR']
 };
