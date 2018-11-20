1d75a4a48fbe8bd28668c4ef0440fbacf0275ef9
uladbohdan <vlbohdan@gmail.com>
2018-11-20 11:12:00 +0300

    Initial commit replacing google chart with mapbox

--- map.js
+++ map.js
@@ -2,20 +2,99 @@ var common_data = intersection(users.ulad.countries, users.marie.countries);
 var unique_first = difference(users.ulad.countries, common_data);
 var unique_second = difference(users.marie.countries, common_data);
 
-google.charts.load("current", {
-  packages: ["geochart"]
+// google.charts.load("current", {
+//   packages: ["geochart"]
   // Note: you will need to get a mapsApiKey for your project.
   // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
   // mapsApiKey: "AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY"
-});
-google.charts.setOnLoadCallback(renderPage);
+// });
+// google.charts.setOnLoadCallback(renderPage);
 
-function renderPage() {
-  drawMaps();
-  renderNames();
-  renderCounters();
-  renderCountryLists();
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoidWxhZGJvaGRhbiIsImEiOiJjam9kMDQ1NzYxOTYyM3FvanhpOXE1cDIzIn0.JiXb8lR9e53GqZz51PZdaQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-99.9, 41.5],
+    zoom: 2
+});
+
+map.on('load', function() {
+
+    map.addSource('belarus', {
+        type: 'vector',
+        url: 'mapbox://uladbohdan.9w37et5v'
+    });
+
+    map.addSource('belarus2', {
+        type: 'vector',
+        url: 'mapbox://uladbohdan.axo35jf3'
+    });
+
+    var all_regions = {};
+
+    unique_first.forEach(function(country) {
+      normalized = country;
+      // if (country in countryToCodeMap) {
+      //   normalized = countryToCodeMap[country]
+      // }
+      all_regions[normalized] = ["rgb", 78, 179, 211];
+    })
+    unique_second.forEach(function(country) {
+      normalized = country;
+      // if (country in countryToCodeMap) {
+      //   normalized = countryToCodeMap[country]
+      // }
+      all_regions[normalized] = ["rgb", 254, 217, 118]
+    })
+    common_data.forEach(function(country) {
+      normalized = country;
+      // if (country in countryToCodeMap) {
+      //   normalized = countryToCodeMap[country]
+      // }
+      all_regions[normalized] = ["rgb", 204, 51, 51]
+    })
+
+    var all1 = ["match", ["get", "iso_3166_2"]]
+    var all2 = ["match", ["get", "NAME"]]
+    for (var country in all_regions) {
+      all1.push(country, all_regions[country])
+      all2.push(country, all_regions[country])
+    }
+    all1.push(["rgba", 0, 0, 0, 0])
+    all2.push(["rgba", 0, 0, 0, 0])
+
+    // Add layer from the vector tile source with data-driven style
+    map.addLayer({
+        "id": "states-join",
+        "type": "fill",
+        "source": "belarus",
+        "source-layer": "ne_10m_admin_1_states_provinc-2kzgpu",
+        "paint": {
+            "fill-color": all1
+        }
+    });
+
+    map.addLayer({
+        "id": "states-join2",
+        "type": "fill",
+        "source": "belarus2",
+        "source-layer": "ne_10m_admin_0_countries-1tntle",
+        "paint": {
+            "fill-color": all2
+        }
+    }, 'waterway-label');
+
+    renderNames();
+    renderCounters();
+    renderCountryLists();
+});
+
+// function renderPage() {
+//   // drawMaps();
+//   renderNames();
+//   renderCounters();
+//   renderCountryLists();
+// }
 
 function drawMaps() {
   drawMap("Country", "world", "countries", "world_div");
