2b3769e82badb720fa61cf6a321f643165807920
uladbohdan <vlbohdan@gmail.com>
2019-01-29 20:09:49 +0300

    [Marie] Georgia

--- data.js
+++ data.js
@@ -22,7 +22,8 @@ var countries = {
     'Denmark',
     'Finland',
     'TR',
+    'GE',
   ],
-  ulad: ['GB', 'PT', 'US', 'US-CA', 'US-WA', 'MX', 'CU', 'RU-KDA', 'GE', 'US-NV', 'US-MA', 'US-NY', 'US-OR', 'Canada', 'CA-BC', 'US-HI', 'CA-ON', 'CA-QC', 'RU-MOS', 'RU-KGD', 'RU-TA', 'CY'],
+  ulad: ['GB', 'PT', 'US', 'US-CA', 'US-WA', 'MX', 'CU', 'RU-KDA', 'US-NV', 'US-MA', 'US-NY', 'US-OR', 'Canada', 'CA-BC', 'US-HI', 'CA-ON', 'CA-QC', 'RU-MOS', 'RU-KGD', 'RU-TA', 'CY'],
   marie: ['IT', 'NO', 'RU-KR', 'RU-MUR', 'RU-PSK', 'RU-NGR']
 };
